6d454b46ab1a1d531f6c14b7b8dfceb0e58ae939
viveks77 <viveksurya771@gmail.com>
2024-05-05 18:03:48 +0530

    chore: updated index page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/common/ThemeProvider";
 import Navbar from "@/components/Navbar";
 import { TRPCReactProvider } from "@/trpc/react";
 import { Toaster } from "@/components/ui/toaster";
-import NextTopLoader from 'nextjs-toploader';
+import NextTopLoader from "nextjs-toploader";
 import "simplebar-react/dist/simplebar.min.css";
 
 const fontSans = Inter({
 	subsets: ["latin"],
 	variable: "--font-sans",
 });
+
+const poppinsFont = Poppins({weight: ["100", "200", "300", "400", "500", "600", "700"],subsets: ["latin"]})
 
 export const metadata: Metadata = {
 	title: "Sphere.",
@@ -26,7 +28,7 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={cn("antialiased grainy", fontSans.variable)}>
+			<body className={cn("antialiased grainy", poppinsFont.className)}>
 				<TRPCReactProvider>
 					<ThemeProvider attribute="class" defaultTheme="system">
 						<NextTopLoader />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { buttonVariants } from "@/components/ui/button";
 import MaxwidthWrapper from "@/components/common/MaxwidthWrapper";
 import { ArrowRight } from "lucide-react";
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function Home() {
 	return (
@@ -10,11 +11,22 @@ export default function Home() {
 				Chat with your <span className="text-blue-600">documents</span> in seconds.
 			</h1>
 			<p className="mt-5 max-w-prose text-muted-foreground sm:text-lg">
-				Sphere<span className="text-red-500">.</span> enables seamless conversations with any PDF file. Just upload your document and dive into interactive discussions in seconds.
+				Sphere<span className="text-red-500">.</span> enables seamless conversations with any PDF file. Just upload your
+				document and dive into interactive discussions in seconds.
 			</p>
 			<Link className={buttonVariants({ size: "lg", className: "mt-5" })} href="/dashboard">
 				Get Started <ArrowRight className="ml-2 h-5 w-5" />
 			</Link>
+			<div className="mt-14 border-8 border-border rounded-xl">
+				<img alt="dashboard" src="/dashboard_dark.png" className="rounded-lg" />
+			</div>
+			<div className="mt-14">
+				<h2 className="max-w-4xl text-2xl md:text-3xl lg:text-4xl font-bold">It all starts with your information sources</h2>
+				<p className="mt-2 max-w-prose text-muted-foreground sm:text-lg">
+					Generate an AI assistant on top of any document so you can quickly find, summarize and understand info. No more
+					endless skimming.
+				</p>
+			</div>
 		</MaxwidthWrapper>
 	);
 }
